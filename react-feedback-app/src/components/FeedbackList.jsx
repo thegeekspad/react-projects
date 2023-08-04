8d68fa50208f51e184abd86a5e358f16563b394d
thegeekspad <thegeekspad@gmail.com>
2023-08-04 08:09:49 +0100

    Added card shared component and defined prop types

--- react-feedback-app/src/components/FeedbackList.jsx
+++ react-feedback-app/src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
 const FeedbackList = ({ feedback }) => {
@@ -13,6 +14,16 @@ const FeedbackList = ({ feedback }) => {
       ))}
     </div>
   );
+};
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default FeedbackList;

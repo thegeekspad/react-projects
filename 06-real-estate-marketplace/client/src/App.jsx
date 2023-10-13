e0044e4d93baf5950e9a0b9fc6e2f44883e81e9d
thegeekspad <thegeekspad@gmail.com>
2023-10-13 08:51:05 +0100

    Create pages placeholders and routes

--- 06-real-estate-marketplace/client/src/App.jsx
+++ 06-real-estate-marketplace/client/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

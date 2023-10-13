a2eb1496991d29d082d693d8965a13aeae1cfc10
thegeekspad <thegeekspad@gmail.com>
2023-10-13 07:50:35 +0100

    Folder reorg, Add real estate marketplace project

--- 05-todo/src/components/footer/Footer.jsx
+++ 05-todo/src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import './Footer.css';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <>
       <footer className="bg-blue-500 p-4 mt-2">
         <div className="container mx-auto">
-          <p className="text-white text-center">© 2023 All rights reserved</p>
+          <p className="text-white text-center">© {year} All rights reserved</p>
         </div>
       </footer>
     </>

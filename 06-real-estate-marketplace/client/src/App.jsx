a030e00b5cffe82fdf23cff26ad5d51b74ad234b
thegeekspad <thegeekspad@gmail.com>
2023-10-13 21:26:16 +0100

    Create header component

--- 06-real-estate-marketplace/client/src/App.jsx
+++ 06-real-estate-marketplace/client/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />

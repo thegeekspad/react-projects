9c2687f524a9eba2939d18d9ec68ef35b48d2a78
thegeekspad <thegeekspad@gmail.com>
2023-10-16 18:01:37 +0100

    Integrate signup with backend API

--- 06-real-estate-marketplace/client/src/pages/SignUp.jsx
+++ 06-real-estate-marketplace/client/src/pages/SignUp.jsx
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  // console.log(formData);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    // Connect to backend API using fetch request
+    const response = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    console.log('User signup successful');
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={submitHandler}>
         <input
           type="text"
           placeholder="Enter username"
-          className='border p-3 rounded-lg id="username'></input>
+          className="border p-3 rounded-lg"
+          id="name"
+          onChange={handleChange}
+        />
         <input
           type="text"
           placeholder="Enter email id"
-          className='border p-3 rounded-lg id="email'></input>
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
         <input
           type="password"
           placeholder="Enter password"
-          className='border p-3 rounded-lg id="password'></input>
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
         <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-50">
           Sign Up
         </button>
--- 06-real-estate-marketplace/server/controllers/authController.js
+++ 06-real-estate-marketplace/server/controllers/authController.js
@@ -6,21 +6,21 @@ export const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     // Validate user input
-    if (!name || !email || !password) {
-      return res.status(400).send('All fields required');
-    }
+    // if (!name || !email || !password) {
+    //   return res.status(400).send('All fields required');
+    // }
 
-    // Check if user name already exists
-    const existingUserName = await User.findOne({ name });
-    if (existingUserName) {
-      return res.status(400).send(`${name} username already exists`);
-    }
+    // // Check if user name already exists
+    // const existingUserName = await User.findOne({ name });
+    // if (existingUserName) {
+    //   return res.status(400).send(`${name} username already exists`);
+    // }
 
-    // Check if user email already exists
-    const existingUserEmail = await User.findOne({ email });
-    if (existingUserEmail) {
-      return res.status(400).send(`${email} user email already exists`);
-    }
+    // // Check if user email already exists
+    // const existingUserEmail = await User.findOne({ email });
+    // if (existingUserEmail) {
+    //   return res.status(400).send(`${email} user email already exists`);
+    // }
 
     // Create new user
     const newUser = await User.create({
